a2c7e11b9b68125c04c153a5e595bd50107b8344
valerioneri <neri.1754516@studenti.uniroma1.it>
2019-08-01 15:29:48 +0200

    Fixed memory leak in clean_request

--- general/lib/socket/socket.c
+++ general/lib/socket/socket.c
@@ -138,10 +138,11 @@ void clean_request(char *path, char *buf, struct ThreadArgs *args) {
     }
     int ret = 0;
     free(args->ip_client);
-    free(args);
     if (args->configs.mode_concurrency == M_THREAD) {
+        free(args);
         pthread_exit(&ret);
     } else { // mode_concurrency == M_PROCESS
+        free(args);
         exit(ret);
     }
 }
