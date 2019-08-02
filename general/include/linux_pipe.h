750482ef7df0c34c8e8f85328f2f14ef4fcaac57
andrea <bacciu.1747105@studenti.uniroma1.it>
2019-08-02 17:58:15 +0200

    Log works with single process without locks

--- general/include/linux_pipe.h
+++ general/include/linux_pipe.h
@@ -2,8 +2,10 @@
 #define GOPHERLINUX_LINUX_PIPE_H
 
 void socket_pipe_log(char* path, struct ThreadArgs *args, int dim_file_to_send);
-int socket_pipe_log_server(char *path, struct ThreadArgs *args, int dim_file_to_send, int *fd_pipe_log) ;
-void socket_pipe_new_process();
+int socket_pipe_log_server(char *path, struct ThreadArgs *args, int dim_file_to_send, int fd_pipe_log_write) ;
+void socket_pipe_multiple_process(int *fd_pipe);
+void socket_pipe_single_process(int *fd_pipe);
+int socket_pipe_log_server_single_process(char *path, struct ThreadArgs *args, int dim_file_to_send, int fd_pipe_log_write);
 void socket_pipe_new_process2();
 void socket_pipe_new_process3();
 
